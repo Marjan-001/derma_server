7696eec63e4fe9b48ce0d14ee922852133d8739e
Marjan-001 <marjan199721@gmail.com>
2023-06-26 22:06:15 +0600

    jwt token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app= express();
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT||5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -79,6 +80,18 @@ app.post('/users',async(req,res)=>{
   const user=req.body;
   const result = await userCollection.insertOne(user);
   res.send(result)
+})
+
+app.get('/jwt', async(req,res)=>{
+
+  const email= req.query.email;
+  const query={email:email};
+  const user = await userCollection.findOne(query);
+  if(user){
+    const token = jwt.sign({email},process.env.ACCESS_TOKEN,{expiresIn:'1h'})
+    return res.send({accessToken:token})
+  }
+  res.status(403).send({accessToken:''})
 })
 }
 finally{
